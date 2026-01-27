7f334a8d672ed3b264b370241f4c80c45252cf01
Luh-code <Lasse@hueffler.de>
2026-01-27 15:30:59 +0100

    i hate svg

--- src/routes/graph/SourceNode.svelte
+++ src/routes/graph/SourceNode.svelte
@@ -8,6 +8,9 @@
   let { positionAbsoluteX, positionAbsoluteY }: NodeProps = $props();
 
   let rot = $state(0);
+  let arms = $state([
+    { id: 1, angle: 0 } // Neumorphic colors are usually close to the BG
+  ]);
 </script>
 
 <div>
@@ -57,14 +60,45 @@
     style:left="-8px"
     class="-z-1 absolute border-23 border-neumorphic-bg rounded-4xl">
   </div>
-  <svg viewBox="-100 -100 200 200" width="300" height="300">
-  <circle cx="0" cy="0" r="90" fill="#e9ecef" />
-  
-  <g id="arm-template" transform="rotate(45)">
-    <rect x="-10" y="-80" width="20" height="80" fill="#c00" />
-    <circle cx="0" cy="0" r="25" fill="#c00" stroke="#000" stroke-width="2" />
-  </g>
-</svg>
+
+  <svg viewBox="-100 -100 200 200" width="20" height="20">
+    <defs>
+      <filter id="neumorphic_inset_red" filterUnits="userSpaceOnUse" x="-100%" y="-100%" width="300%" height="300%">
+        <feOffset dx="6" dy="6" in="SourceAlpha" result="offset" />
+        <feGaussianBlur stdDeviation="3" in="offset" result="blur" />
+        <feComposite operator="out" in="SourceAlpha" in2="blur" result="inverse" />
+        <feFlood flood-color="#923737" flood-opacity=".6" result="color" />
+        <feComposite operator="in" in="color" in2="inverse" result="shadow" />
+
+        <feOffset dx="-6" dy="-6" in="SourceAlpha" result="offsetLight" />
+        <feGaussianBlur stdDeviation="3" in="offsetLight" result="blurLight" />
+        <feComposite operator="out" in="SourceAlpha" in2="blurLight" result="inverseLight" />
+        <feFlood flood-color="#ff0000" flood-opacity=".6" result="colorLight" />
+        <feComposite operator="in" in="colorLight" in2="inverseLight" result="highlight" />
+
+        <feMerge>
+          <feMergeNode in="SourceGraphic" />
+          <feMergeNode in="shadow" />
+          <feMergeNode in="highlight" />
+        </feMerge>
+      </filter>
+    </defs>
+
+    <circle cx="0" cy="0" r="95" fill="#f0f0f0" />
+
+    {#each arms as arm (arm.id)}
+      <g transform="rotate({arm.angle})" filter="" >
+        <path 
+          d="M -8 -80 
+            A 8 8 0 0 1 8 -80 
+            L 8 -15 
+            A 22 22 0 1 1 -8 -15 
+            Z" 
+          fill="var(--color-deep-red)" 
+        />
+      </g>
+    {/each}
+  </svg>
 </div>
 
 <!--Handle type="source" position={Position.Left} />
